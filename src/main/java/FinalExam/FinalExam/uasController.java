9aee61b7d7930cc80be47d20550fd6cde9be233b
Activity_4 <egyputra78@gmail.com>
2023-01-13 09:28:20 +0700

    Menambahkan method untuk membaca data

--- src/main/java/FinalExam/FinalExam/uasController.java
+++ src/main/java/FinalExam/FinalExam/uasController.java
@@ -4,10 +4,45 @@
  */
 package FinalExam.FinalExam;
 
+import java.util.ArrayList;
+import java.util.List;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author asus
  */
+@RestController
+@CrossOrigin
 public class uasController {
     
+    Person data = new Person();
+    PersonJpaController pbjp = new PersonJpaController();
+    
+     @RequestMapping( value = "/GET/{id}",method= RequestMethod.GET )
+    public String getNameById(@PathVariable("id") int id){
+        try{
+            data = pbjp.findPerson(id);
+        }catch (Exception e){
+            
+        }
+        return data.getNama();
+    }
+    
+    @RequestMapping(value = "/GET" ,method= RequestMethod.GET,consumes = APPLICATION_JSON_VALUE )
+    public List<Person> getAll(){
+        List<Person> dummy = new ArrayList();
+        try {
+            dummy = pbjp.findPersonEntities();
+        } catch (Exception e) {
+            dummy = List.of();
+                    
+        }
+        return dummy;
+    }
 }
